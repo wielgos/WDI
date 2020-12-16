3d31e38e6191ace4ecf3a67062e864ef2d530e2c
Mikolaj <wielgosmikolaj@gmail.com>
2020-12-16 19:17:57 +0100

    update

---
+++ zestaw6/zad11.py
@@ -0,0 +1,25 @@
+# Zadanie 11. Dana jest tablica T[N]. Proszę napisać program zliczający liczbę “enek” o określonym iloczynie.
+
+
+def f(t,q,n,p=0,used=[]):
+    if n==1:
+        if t[p]==q:
+            used = used + [t[p]]
+            print(used)
+            return 1
+        else:
+            return 0
+    if p==len(t):
+        return 0
+    if q%t[p]==0:
+        return f(t,q//t[p],n-1,p+1,used + [t[p]]) or f(t,q,n,p+1)
+    else:
+        return f(t,q,n,p+1)
+
+
+t=[1,2,3,4]
+
+print(f(t,6,2))
+
+
+
